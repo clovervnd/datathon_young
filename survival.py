b04f5e8a2979f2978c33a3a9f0dc8352ce82cc47
robovolt <robovolt@robovoltui-MacBookPro.local>
2019-09-20 21:00:30 +0900

    initial commit

---
+++ survival.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+# from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+import dataloader as dl
+# Training settings
+batch_size = 64
+n_class=3
+
+
+train_loader = dl.get_dataloader(is_train=True, batch_size=batch_size)
+test_loader = dl.get_dataloader(is_train=True, batch_size=batch_size)
+
+
+if 0:
+# MNIST Dataset
+    train_dataset = datasets.MNIST(root='./data/',
+                                   train=True,
+                                   transform=transforms.ToTensor(),
+                                   download=True)
+
+    test_dataset = datasets.MNIST(root='./data/',
+                                  train=False,
+                                  transform=transforms.ToTensor())
+
+# Data Loader (Input Pipeline)
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.mp = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(17, 200)
+        self.fc2 = nn.Linear(200, 400)
+        self.fc3 = nn.Linear(400, 300)
+        self.fc4 = nn.Linear(300, 100)
+        self.fc5 = nn.Linear(100, 10)
+        self.fc_final = nn.Linear(10, n_class)
+
+    def forward(self, x):
+        in_size = x.size(0)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = F.relu(self.fc5(x))
+        x = self.fc_final(x)
+        return F.log_softmax(x)
+
+
+model = Net()
+
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target.long())
+        print (data, target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        data, target = Variable(data, volatile=True), Variable(target.long())
+        output = model(data)
+        # sum up batch loss
+        test_loss += F.nll_loss(output, target, size_average=False).data[0]
+        # get the index of the max log-probability
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1, 10):
+    train(epoch)
+    test()
