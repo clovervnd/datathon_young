e66ef7c90cdec3fd579d2f513243c7aa9cbac741
Joonki <jkhong12000@gmail.com>
2019-09-21 17:22:12 +0900

    update

--- single_machine/survival.py
+++ single_machine/survival.py
@@ -21,9 +21,6 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        # self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        # self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        # self.mp = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(68, 200)
         self.fc2 = nn.Linear(200, 100)
         self.fc3 = nn.Linear(100, 10)
